3822807d9e779ba04397a91c85ac067fe22a2334
tanvir43 <raihncse43@gmail.com>
2021-11-27 12:23:09 +0600

    added a basic higher order function

---
+++ decorator/higher_order_function.py
@@ -0,0 +1,13 @@
+# When a python function take another function as an argument
+# then that function is called higher order function
+
+
+# A higher order function
+def higher_order_function(func):
+    return func()
+
+def my_func():
+        print("This is my function")
+
+if __name__ == "__main__":
+    higher_order_function(my_func)
